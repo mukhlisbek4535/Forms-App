566a211bde6bfaeaa1e958f24353f7e70f21cc05
Mukhlisbek <mukhlisbekkirgizov@gmail.com>
2025-04-28 14:45:19 +0500

    Protected Route

--- frontend/components/ProtectedRoute.jsx
+++ frontend/components/ProtectedRoute.jsx
@@ -2,11 +2,10 @@ import { useAuth } from "../src/context/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children, roles = [] }) => {
-  const { isAuthenticated, user } = useAuth();
+  const { isAuthenticated } = useAuth();
   const location = useLocation();
 
-  if (!isAuthenticated)
-    return <Navigate to="/login" state={{ from: location }} replace />;
+  if (!isAuthenticated) return <Navigate to="/login" />;
 
   // if (roles.length > 0 && !roles.includes(user?.role))
   //   return <Navigate to="/unauthorized" replace />;
