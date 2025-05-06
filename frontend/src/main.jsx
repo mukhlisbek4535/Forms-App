c68d5065202c15324970913bcdf5de1d58c0828c
Mukhlisbek <mukhlisbekkirgizov@gmail.com>
2025-05-06 11:45:57 +0500

    changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -73,7 +73,7 @@ const Main = () => {
           path: "/templates",
           children: [
             {
-              path: "",
+              path: "dashboard",
               element: (
                 <ProtectedRoute>
                   <TemplateList />
